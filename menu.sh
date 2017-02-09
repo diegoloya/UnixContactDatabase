27fe6efe618970434c4172eb6d5195d0b3649708
Diego Loya <d_l56@txstate.edu>
2017-02-09 13:12:29 -0600

    Displays the menu

---
+++ menu.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#Display menu
+
+echo "Welcome to my contact database, please select from the following menu:"
+echo "(a) Find a record"
+echo "(b) Add a new record"
+echo "(c) Update a record"
+echo "(d) Remove a record"
+echo "(e) Quit"
+read find
+
+
+
