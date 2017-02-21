649143f6b12111b5925a989159cf04806356b3a6
mar395 <mar395@txstate.edu>
2017-02-20 19:09:22 -0600

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,14 +1,26 @@
 #!/bin/sh
-
-file="file.txt"
-
+#This works for most cases
+#Sucessfully removes single entries
 #First we ask the user which entry they want to delete
-echo -n "Pick an entry to delete: "
+echo "Search for an entry to delete: "
 read deleteName
 
-grep -v deleteName file > tempfile.txt
+grep "$deleteName" file.txt
 
-mv tempfile.txt file.txt
+echo "Is this the entry you want to delete? Y or N"
+read choice
+
+if [ "$choice" = "y" ] || [ "$choice" = "Y" ]; 
+then	
+	grep -v "$deleteName" file.txt > tempfile.txt
+	mv tempfile.txt file.txt
+	echo "Entry removed!"
+
+else
+	echo "File not removed!"
+	exit 0
+
+fi
 
 
 
